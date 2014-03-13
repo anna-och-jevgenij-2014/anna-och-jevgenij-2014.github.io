fdce259d65b475bec4e973dae24eff1d024f4fcb
Jevgenij Tsoi <jevgenij@virtusize.com>
2014-03-13 22:51:10 +0100

    Looking better. =)

--- scripts/main.js
+++ scripts/main.js
@@ -1,1 +1,23 @@
-console.log('\'Allo \'Allo!');
+
+
+$(".images-1-1").backstretch("http://placehold.it/300x300");
+
+$(".images-2-1").backstretch("http://placehold.it/300x300");
+$(".images-2-2").backstretch("http://placehold.it/300x300");
+$(".images-2-3").backstretch("http://placehold.it/300x300");
+
+$(".images-3-1").backstretch("http://placehold.it/300x300");
+
+$(".images-4-1").backstretch("http://placehold.it/300x300");
+
+$(".images-5-1").backstretch("http://placehold.it/600x300");
+$(".images-5-2").backstretch("http://placehold.it/300x300");
+
+$(".images-6-1").backstretch("http://placehold.it/600x600");
+
+$(".images-7-1").backstretch("http://placehold.it/300x300");
+
+$(".images-8-1").backstretch("http://placehold.it/300x300");
+
+
+$('.btn').button();
