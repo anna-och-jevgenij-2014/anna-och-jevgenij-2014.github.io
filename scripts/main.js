0712e673e9315aa0c6fb70ce794b6fe78464b52a
Jevgenij Tsoi <jevgenij@virtusize.com>
2014-03-22 13:16:43 +0100

    Feature: Added Parse integration

--- scripts/main.js
+++ scripts/main.js
@@ -1,4 +1,4 @@
-
+Parse.initialize("sbvjVEY7RR8pDyYAwxQbuBd8qbYyxJPZZ3lcxOf2", "VhUUULfEt0pJaZG8jm0lBIdbJ1IlKaIzjNnd9iNj");
 
 $(".images-1-1").backstretch("http://placehold.it/300x300");
 
@@ -24,4 +24,29 @@ $(".btn").button();
 
 $(".navbar a").smoothScroll({
     offset: 50
+});
+
+
+$('#reply-form').submit(function(){
+    $('#submit').button('loading');
+
+    if($('#email').val()){
+        console.log('invalid');
+        return false;
+    }
+
+    var values = {
+        name: $('#name').val(),
+        anwser: $('#reply-form input[type=radio]:checked').val(),
+        wishes: $('#wishes').val()
+    };
+
+    var WeddingReply = Parse.Object.extend("WeddingReply");
+    var reply = new WeddingReply();
+    reply.save(values).then(function(object) {
+        alert("yay! it worked");
+    });
+
+    return false;
+
 });
