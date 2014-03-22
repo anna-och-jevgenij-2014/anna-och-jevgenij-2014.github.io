d4bef920ec80b16bd061488d4a1452d836c8f48d
Jevgenij Tsoi <jevgenij@virtusize.com>
2014-03-22 14:40:59 +0100

    Feature: added some actual pics and CNAME file

--- scripts/main.js
+++ scripts/main.js
@@ -1,11 +1,5 @@
 Parse.initialize("sbvjVEY7RR8pDyYAwxQbuBd8qbYyxJPZZ3lcxOf2", "VhUUULfEt0pJaZG8jm0lBIdbJ1IlKaIzjNnd9iNj");
 
-$(".images-1-1").backstretch("http://placehold.it/300x300");
-
-$(".images-2-1").backstretch("http://placehold.it/300x300");
-$(".images-2-2").backstretch("http://placehold.it/300x300");
-$(".images-2-3").backstretch("http://placehold.it/300x300");
-
 $(".images-3-1").backstretch("http://placehold.it/300x300");
 
 $(".images-4-1").backstretch("http://placehold.it/300x300");
@@ -19,6 +13,25 @@ $(".images-7-1").backstretch("http://placehold.it/300x300");
 
 $(".images-8-1").backstretch("http://placehold.it/300x300");
 
+$(".images").each(function(index, element){
+
+    var $el = $(element)
+    if($el.data("img")){
+        $el.backstretch($el.data("img"))
+    }
+
+    var images = [];
+    $el.children("span").each(function(index, child){
+        var $el = $(child)
+        if($el.data("img")){
+            images.push($el.data("img"))
+        }
+    });
+    if(images.length > 0){
+        $el.backstretch(images)
+    }
+
+});
 
 $(".btn").button();
 
