082f25825da6aeae00eeddb26b997cc801ae8ab4
Jevgenij Tsoi <jevgenij@virtusize.com>
2014-04-01 22:44:13 +0200

    Added annas changes, added email

--- scripts/main.js
+++ scripts/main.js
@@ -1,17 +1,5 @@
 Parse.initialize("sbvjVEY7RR8pDyYAwxQbuBd8qbYyxJPZZ3lcxOf2", "VhUUULfEt0pJaZG8jm0lBIdbJ1IlKaIzjNnd9iNj");
 
-$(".images-3-1").backstretch("http://placehold.it/300x300");
-
-$(".images-4-1").backstretch("http://placehold.it/300x300");
-
-$(".images-5-1").backstretch("http://placehold.it/600x300");
-$(".images-5-2").backstretch("http://placehold.it/300x300");
-
-$(".images-6-1").backstretch("http://placehold.it/600x600");
-
-$(".images-7-1").backstretch("http://placehold.it/300x300");
-
-$(".images-8-1").backstretch("http://placehold.it/300x300");
 
 $(".images").each(function(index, element){
 
@@ -49,6 +37,7 @@ $('#reply-form').submit(function(){
     }
 
     var values = {
+        email: $('#confirmation').val(),
         name: $('#name').val(),
         anwser: $('#reply-form input[type=radio]:checked').val(),
         wishes: $('#wishes').val()
